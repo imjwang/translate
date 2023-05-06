60b51a2cf890bdbaa73e4da298504082ad9691ee
Jeffrey Wang <ngjaywa@gmail.com>
2023-05-05 20:24:30 -0400

    add debounce

--- src/app/Search.js
+++ src/app/Search.js
@@ -1,20 +1,45 @@
+import { debounce } from "lodash";
 import {
   TextField,
   Card,
   CardActions,
-  Typography,
-  IconButton,
   Stack,
+  CircularProgress,
 } from "@mui/material";
 import GTranslateIcon from "@mui/icons-material/GTranslate";
+import { useState, useEffect, useCallback } from "react";
 
 const Search = () => {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const callApi = useCallback(
+    debounce(async (value) => {
+      // Call your API here    setLoading(true);
+      setLoading(true);
+      console.log(`API call with: ${value}`);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setLoading(false);
+    }, 3000),
+    []
+  );
+
+  useEffect(() => {
+    // Call the debounced function whenever value changes
+    if (text.length >= 3) {
+      callApi(text);
+    }
+  }, [text]);
   return (
     <Card sx={{ p: 2 }}>
       <TextField
         id="Type"
         label="在此输入"
         placeholder="你好"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
         multiline
         variant="outlined"
         fullWidth
@@ -23,8 +48,12 @@ const Search = () => {
         minRows={4}
       />
       <CardActions>
-        <Stack sx={{ width: "100%" }} direction={"row-reverse"}>
-          <GTranslateIcon color="primary" fontSize="large" />
+        <Stack sx={{ width: "100%", height: "7vh" }} direction={"row-reverse"}>
+          {loading ? (
+            <CircularProgress size={30} color="primary" />
+          ) : (
+            <GTranslateIcon color="primary" fontSize="large" />
+          )}
         </Stack>
       </CardActions>
     </Card>
