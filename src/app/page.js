1270d9311357777e9295b43e26ad39b6c2ca40c4
Jeffrey Wang <ngjaywa@gmail.com>
2023-05-05 23:54:22 -0400

    appbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
 "use client";
 import Image from "next/image";
-import { Typography, CssBaseline, Paper, Stack, Card } from "@mui/material";
+import {
+  Typography,
+  CssBaseline,
+  Paper,
+  Stack,
+  Card,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
 import Mic from "./Mic";
 import Search from "./Search";
 import { useState } from "react";
@@ -10,13 +18,20 @@ export default function Home() {
   return (
     <>
       <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">Translate</Typography>
+        </Toolbar>
+      </AppBar>
       <Stack spacing={4} sx={{ m: 4 }}>
         <Card>
           <Search setResult={setResult} />
         </Card>
-        <Paper sx={{ p: 3, mt: 8 }}>
-          <Typography variant="h4">{result}</Typography>
-        </Paper>
+        {result.length > 0 && (
+          <Paper sx={{ p: 3, mt: 8 }}>
+            <Typography variant="h4">{result}</Typography>
+          </Paper>
+        )}
       </Stack>
       <Mic />
     </>
